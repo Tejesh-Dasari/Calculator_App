474fb4c46496198239b7d062fb99b1c9d5496243
Tejesh-Dasari <tejeshdasari29@gmail.com>
2023-01-21 10:58:14 +0530

    Added main function

--- cal.py
+++ cal.py
@@ -31,3 +31,16 @@ while True:
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
+        
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
