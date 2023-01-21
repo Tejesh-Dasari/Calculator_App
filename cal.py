576c9559ac47eb2c4d6bc3d73b013ca6764ef166
Tejesh-Dasari <tejeshdasari29@gmail.com>
2023-01-21 10:54:33 +0530

    Added error handling

--- cal.py
+++ cal.py
@@ -20,3 +20,14 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
